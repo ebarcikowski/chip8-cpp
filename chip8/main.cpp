3fbff5b3e84b2bddec973c0459cf1f03696f9cec
Elliott Barcikowski <ebarcikowski@gmail.com>
2018-05-06 11:21:14 -0600

    Added usage statement.

--- chip8/main.cpp
+++ chip8/main.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char *argv[])
 {
   Chip8SDL chip8;
+  if (argc < 2) {
+    std::cout << "usage: " << argv[0] << " <rom>\n";
+    exit(1);
+  }
   assert(argc > 1);
   chip8.Load(argv[1]);
   chip8.Run();
